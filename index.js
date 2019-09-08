3e284d40486a41f0df0c8333630fba7422a78eca
Nataliya R <n.rudenko@gmail.com>
2019-09-08 17:26:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const driver = 'bob';
 console.log('this code was called');
 console.log('bob');
-console.log('driver', 'is the driver variable naem');
+console.log('driver', 'is the driver variable name');
